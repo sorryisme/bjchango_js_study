969b2990e1eafc1f5d3b8e5349d86f8ff58ef6df
sorryisme <wivipp39@gmail.com>
2021-01-27 11:47:18 +0900

    add:mutation add

--- server/index.js
+++ server/index.js
@@ -7,22 +7,40 @@ const config = require('./config/db_config.json');
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool(config);
 
-const getQuery = async ( _param ) => {
-    let connection = await pool.getConnection(async conn => conn);
+const execQueryWithParam = async ( query, args ) => {
+    let connection ;
     try{ 
-        const rows = await connection.query(_param);
-        connection.release();
+        connection = await pool.getConnection();
+        const rows = await connection.query(query, args);
         return rows[0];
     } catch ( err ) {
         console.log(err);
-        connection.release();
         return false;
+    } finally {
+        if(connection) connection.release();
+    }
+};
+
+const execQuery = async ( query ) => {
+    let connection;
+    try{ 
+        connection = await pool.getConnection();
+        const rows = await connection.query(query);
+        return rows[0];
+    } catch ( err ) {
+        console.log(err);
+        return false;
+    } finally {
+        if(connection) connection.release();
     }
 };
 
 const typeDefs = gql`
     type Query {
-        userFind: UserType
+        users : [UserType]
+    }
+    type Mutation {
+        signUp(id:String, name:String, email:String) : UserType
     }
     type UserType {
         id: String
@@ -33,10 +51,26 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: { 
-        userFind : async () => {
+        users : async () => {
             const query = "SELECT id,name,email FROM TB_USER";
-            let sendData = await getQuery(query)
-            return sendData[0];
+            const sendData = await execQuery(query)
+            return sendData;
+        }
+    },
+    Mutation: { 
+        signUp : async (_, params) => {
+            
+            let data = [];
+            for(let prop in params ) {
+                data.push(params[prop]);
+            }
+            const query = "INSERT INTO TB_USER(id,name,email) VALUES(?, ?, ?)";
+            const resultData = await execQueryWithParam(query, data);
+            
+            if(resultData.affectedRows > 0) {
+                return params;
+            }
+            return '';
         }
     }
 }
